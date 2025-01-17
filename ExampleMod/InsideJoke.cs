326cd741afe3c1af6f7ccbeb2d4fd4fc42bbc560
RubberDuckShobe <42943070+RubberDuckShobe@users.noreply.github.com>
2025-01-17 22:35:16 +0100

    Only process data of selected user

--- ExampleMod/InsideJoke.cs
+++ ExampleMod/InsideJoke.cs
@@ -15,12 +15,12 @@ public class InsideJoke : ResoniteMod {
 	public override string Link => "https://github.com/RubberDuckShobe/InsideJokeReso/";
 
 	[AutoRegisterConfigKey]
-	private static readonly ModConfigurationKey<string> userId = new ModConfigurationKey<string>("userId", "User ID", () => "U-m1nt-");
+	private static readonly ModConfigurationKey<string> userId = new ModConfigurationKey<string>("userName", "Username", () => "R4i");
 	[AutoRegisterConfigKey]
 	private static readonly ModConfigurationKey<string> server = new ModConfigurationKey<string>("server", "Server IP", () => "127.0.0.1");
 	[AutoRegisterConfigKey]
 	private static readonly ModConfigurationKey<int> port = new ModConfigurationKey<int>("port", "Port", () => 4649);
-	private static ModConfiguration Config; //If you use config settings, this will be where you interface with them.
+	private static ModConfiguration Config;
 
 	private static User selectedUser;
 
@@ -33,15 +33,15 @@ public class InsideJoke : ResoniteMod {
 		harmony.PatchAll();
 
 		ws = new(new Uri("ws://" + Config.GetValue(server) + ":" + Config.GetValue(port)));
-		
+
 		ws.Start();
 		ws.ReconnectionHappened.Subscribe(info => Msg($"Reconnection happened, type: {info.Type}"));
 		userId.OnChanged += (newValue) => {
-			Msg("Username changed to " + newValue);
+			Msg("Target username changed to " + newValue);
 			foreach (User user in Engine.Current.WorldManager.FocusedWorld.AllUsers) {
-				Msg("iterating through user " + user.UserID);
+				Debug("iterating through user " + user.UserID);
 				if ((string)newValue == user.UserID) {
-					Msg("User found");
+					Debug("User found");
 					selectedUser = user;
 
 					break;
@@ -53,25 +53,27 @@ public class InsideJoke : ResoniteMod {
 	[HarmonyPatch(typeof(OpusStream<MonoSample>), "DecodeSamples")]
 	class OpusStream_DecodeSamples_Patch {
 		static void Postfix(OpusStream<MonoSample> __instance, ref float[] buffer, BitBinaryReaderX reader) {
-			Msg("Decoding " + buffer.Length + " bytes from " + __instance.User.UserName + " (" + __instance.Name + ")");
+			if (__instance.User.UserName == selectedUser.UserName) {
+				Debug("Decoding " + buffer.Length + " bytes from " + __instance.User.UserName + " (" + __instance.Name + ")");
 
-			for (int i = 0; i < buffer.Length; i++) {
-				buffer[i] = MathX.Clamp(buffer[i], -1, 1);
+				for (int i = 0; i < buffer.Length; i++) {
+					buffer[i] = MathX.Clamp(buffer[i], -1, 1);
+				}
+
+				Int16[] intData = new Int16[buffer.Length];
+				Byte[] bytesData = new Byte[buffer.Length * 2];
+				int rescaleFactor = 32767;
+				for (int i = 0; i < buffer.Length; i++) {
+					intData[i] = (short)(((float)buffer[i]) * rescaleFactor);
+					Byte[] byteArr = new Byte[2];
+					byteArr = BitConverter.GetBytes(intData[i]);
+					byteArr.CopyTo(bytesData, i * 2);
+				}
+
+				Debug("Sending " + bytesData.Length + " bytes");
+
+				ws.Send(bytesData);
 			}
-
-			Int16[] intData = new Int16[buffer.Length];
-			Byte[] bytesData = new Byte[buffer.Length * 2];
-			int rescaleFactor = 32767;
-			for (int i = 0; i < buffer.Length; i++) {
-				intData[i] = (short)(((float)buffer[i]) * rescaleFactor);
-				Byte[] byteArr = new Byte[2];
-				byteArr = BitConverter.GetBytes(intData[i]);
-				byteArr.CopyTo(bytesData, i * 2);
-			}
-
-			Msg("Sending " + bytesData.Length + " bytes");
-
-			ws.Send(bytesData);
 		}
 	}
 }
